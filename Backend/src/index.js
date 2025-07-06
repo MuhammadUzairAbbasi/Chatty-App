db7d775d92219d166bcb3350a01bc9d7fc08aecf
Uzairabbasi25 <k214548@nu.edu.pk>
2025-07-06 17:54:09 +0500

    Deployment link added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,6 +14,10 @@ const port = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
 
+const FrontendUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5173"
+    : process.env.FRONTEND_URL;
 // allow to extract data from req.body
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +25,7 @@ app.use(express.raw());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: FrontendUrl,
     credentials: true,
   })
 );
